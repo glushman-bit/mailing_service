8cfb3703b4b520e1d3bcb27ae32b0c866d64f9de
Ivan Glushenkov <glushman966@gmail.com>
2026-05-24 18:12:46 +0300

    create admin.py for Recipient, Message, Mailing

--- mailing_service/admin.py
+++ mailing_service/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from mailing_service.models import Recipient, Message, Mailing
 
-# Register your models here.
+
+@admin.register(Recipient)
+class RecipientAdmin(admin.ModelAdmin):
+    list_display = ('email', 'full_name', 'comment')
+    search_fields = ('email', 'full_name', 'comment')
+    list_filter = ('email',)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('title','content',)
+    search_fields = ('title', 'content',)
+    list_filter = ('title',)
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('message', 'status')
+    search_fields = ('message', 'status')
+    list_filter = ('message', 'status')
