9466bc3ff3495905fc5d31b80648bcc1472362a2
Ivan Glushenkov <glushman966@gmail.com>
2026-05-25 13:55:23 +0300

    create app users

---
+++ users/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "email",
+    )
+    exclude = ("password",)
+    search_fields = ("email",)
