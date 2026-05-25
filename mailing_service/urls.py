0af42c9bf6582f2ad0566daca62d14ddf8f02d18
Ivan Glushenkov <glushman966@gmail.com>
2026-05-25 22:49:31 +0300

    update context MainPageView, create RecipientListView, MessageListView, MailingListView and urls for them

--- mailing_service/urls.py
+++ mailing_service/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from mailing_service.views import MainPageView
+from mailing_service.views import MainPageView, RecipientListView, MessageListView, MailingListView
 from mailing_service.apps import MailingServiceConfig
 
 
@@ -7,4 +7,9 @@ app_name = MailingServiceConfig.name
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='main_page'),
+    path('recipients/', RecipientListView.as_view(), name='recipients_list'),
+
+    path('messages/', MessageListView.as_view(), name='messages_list'),
+
+    path('mailings/', MailingListView.as_view(), name='mailings_list'),
 ]
--- mailing_service/views.py
+++ mailing_service/views.py
@@ -1,7 +1,51 @@
+from enum import unique
+
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from mailing_service.models import Recipient, Message, Mailing
 
 
 class MainPageView(TemplateView):
-    template_name = "main_page.html"
+    """ Класс вывода главной страницы """
+    template_name = "mailing_service/main_page.html"
+
+    def get_context_data(self, **kwargs):
+        """ Добавление данных на главную страницу """
+        context = super().get_context_data(**kwargs)
+
+        print(context)
+
+        recipients = Recipient.objects.all()
+        context["recipients_all"] = len(list(recipient for recipient in recipients))
+        messages = Message.objects.all()
+        context["messages_all"] = len(list(message for message in messages))
+        mailings = Mailing.objects.all()
+        context["mailings_all"] = len(list(mailing for mailing in mailings))
+
+        return context
+
+
+class RecipientListView(ListView):
+    model = Recipient
+    template_name = "mailing_service/recipients_list.html"
+    context_object_name = "page_object"
+    paginate_by = 10
+    ordering = ["created_at"]
+
+
+class MessageListView(ListView):
+    model = Message
+    template_name = "mailing_service/messages_list.html"
+    context_object_name = "page_object"
+    paginate_by = 10
+    ordering = ["created_at"]
+
+
+class MailingListView(ListView):
+    model = Mailing
+    template_name = "mailing_service/mailings_list.html"
+    context_object_name = "page_object"
+    paginate_by = 10
+    ordering = ["created_at"]
 
