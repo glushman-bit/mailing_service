cb4724a8ad4e35ca9f18c6a4438c38640fab3057
Ivan Glushenkov <glushman966@gmail.com>
2026-06-01 00:34:09 +0300

    update mailing_service/views inc_navbar.html, mailings_list.html, messages_list.html, recipients_list.html for permissions

--- mailing_service/views.py
+++ mailing_service/views.py
@@ -1,10 +1,13 @@
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, redirect
+from django.http import HttpResponseForbidden
+from django.utils import timezone
 
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.views import View
-from django.db.models import Q
+from django.db.models import Q, Count
 
 from mailing_service.models import Recipient, Message, Mailing, MailingAttempt
 from mailing_service.forms import RecipientForm, MessageForm, MailingForm
@@ -12,7 +15,7 @@ from mailing_service.services import start_mailing
 
 
 class MainPageView(TemplateView):
-    """ Класс вывода главной страницы """
+    """ Класс вывода главной страницы с разграничением статистики администратора и пользователей"""
     template_name = "mailing_service/main_page.html"
 
     def get_queryset(self):
@@ -23,36 +26,63 @@ class MainPageView(TemplateView):
     def get_context_data(self, **kwargs):
         """ Добавление данных на главную страницу """
         context = super().get_context_data(**kwargs)
+        current_user = self.request.user
 
-        recipients = Recipient.objects.all()
-        context["recipients_all"] = len([recipient for recipient in recipients])
-        latest_recipients = Recipient.objects.order_by('-id')[:3]
-        context["recipients_last"] = [message for message in latest_recipients]
+        if current_user.is_authenticated:
 
-        messages = Message.objects.all()
-        context["messages_all"] = len([message for message in messages])
-        latest_messages = Message.objects.order_by('-id')[:3]
-        context["messages_last"] = [message for message in latest_messages]
+            if self.request.user.is_superuser or self.request.user.is_staff:
+                user_recipients = Recipient.objects.all()
+                user_messages = Message.objects.all()
+                user_mailings = Mailing.objects.all()
+                user_attempt = MailingAttempt.objects.all()
 
-        mailings = Mailing.objects.all()
-        context["mailings_all"] = len([mailing for mailing in mailings])
-        context["mailings_create"] = len([mailing for mailing in mailings if mailing.status == "Создана"])
-        context["mailings_running"] = len([mailing for mailing in mailings if mailing.status == "Запущена"])
-        context["mailings_completed"] = len([mailing for mailing in mailings if mailing.status == "Завершена"])
+            else:
+                user_recipients = Recipient.objects.filter(owner=current_user)
+                user_messages = Message.objects.filter(owner=current_user)
+                user_mailings = Mailing.objects.filter(owner=current_user)
+                user_attempt = MailingAttempt.objects.filter(mailing__owner=current_user)
 
-        sent_mailings = MailingAttempt.objects.all()
+            context["recipients_all"] = user_recipients.count()
+            context["recipients_last"] = user_recipients.order_by("-id")[:3]
 
-        if self.request.user.is_authenticated:
-            context["sent_mailings_success"] = len([mailing for mailing in sent_mailings if mailing.status == 'success'])
-            context["sent_mailings_error"] = len([mailing for mailing in sent_mailings if mailing.status == 'failure'])
+            context["messages_all"] = user_messages.count()
+            context["messages_last"] = user_messages.order_by("-id")[:3]
 
-            context["messages_success"] = MailingAttempt.objects.filter(
-                status='success',
-                mailing__owner=self.request.user,
-            ).filter(
-                Q(mailing__status=Mailing.STATUS_RUNNING) | Q(mailing__status=Mailing.STATUS_COMPLETED)
-            ).count()
-            return context
+            user_mailings_stats = user_mailings.aggregate(
+                all_count=Count("id"),
+                create_count=Count("id", filter=Q(status=Mailing.STATUS_CREATED)),
+                running_count=Count("id", filter=Q(status=Mailing.STATUS_RUNNING)),
+                completed_count=Count("id", filter=Q(status=Mailing.STATUS_COMPLETED)),
+            )
+            context["mailings_all"] = user_mailings_stats["all_count"]
+            context["mailings_create"] = user_mailings_stats["create_count"]
+            context["mailings_running"] = user_mailings_stats["running_count"]
+            context["mailings_completed"] = user_mailings_stats["completed_count"]
+
+            user_attempt_stats = user_attempt.aggregate(
+                success_all=Count("id", filter=Q(status="success")),
+                failure_all=Count("id", filter=Q(status="failure")),
+                success_filtered=Count("id", filter=Q(
+                    status="success",
+                    mailing__status__in=[Mailing.STATUS_RUNNING, Mailing.STATUS_COMPLETED]
+                ))
+            )
+            context["sent_mailings_success"] = user_attempt_stats["success_all"]
+            context["sent_mailings_error"] = user_attempt_stats["failure_all"]
+            context["messages_success"] = user_attempt_stats["success_filtered"]
+
+        else:
+            context["recipients_all"] = 0
+            context["recipients_last"] = []
+            context["messages_all"] = 0
+            context["messages_last"] = []
+            context["mailings_all"] = 0
+            context["mailings_create"] = 0
+            context["mailings_running"] = 0
+            context["mailings_completed"] = 0
+            context["sent_mailings_success"] = 0
+            context["sent_mailings_error"] = 0
+            context["messages_success"] = 0
 
         return context
 
@@ -65,6 +95,13 @@ class RecipientListView(ListView):
     paginate_by = 10
     ordering = ["created_at"]
 
+    def get_queryset(self):
+        """ Вывод списка получателей рассылки как владельца или как администратора """
+        if self.request.user.is_superuser or self.request.user.is_staff:
+            return Recipient.objects.all()
+
+        return Recipient.objects.filter(owner=self.request.user)
+
 
 class RecipientDetailView(DetailView):
     """ Класс представления детальной информации о получателе рассылки (клиенте) """
@@ -74,7 +111,7 @@ class RecipientDetailView(DetailView):
     success_url = reverse_lazy("mailing_service:recipients_list")
 
 
-class RecipientCreateView(CreateView):
+class RecipientCreateView(LoginRequiredMixin, CreateView):
     """ Класс создания получателя рассылки (клиента) """
     model = Recipient
     form_class = RecipientForm
@@ -85,8 +122,13 @@ class RecipientCreateView(CreateView):
         "object_type": "Recipient",
     }
 
+    def form_valid(self, form):
+        """ Автоматическая привязка пользователя как владельца получателя рассылки """
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
 
-class RecipientUpdateView(UpdateView):
+
+class RecipientUpdateView(LoginRequiredMixin, UpdateView):
     """ Класс редактирования получателя рассылки (клиента) """
     model = Recipient
     form_class = RecipientForm
@@ -98,7 +140,7 @@ class RecipientUpdateView(UpdateView):
     }
 
 
-class RecipientDeleteView(DeleteView):
+class RecipientDeleteView(LoginRequiredMixin, DeleteView):
     """ Класс удаления получателей рассылки (клиентов) """
     model = Recipient
     template_name = "mailing_service/confirm_delete.html"
@@ -117,6 +159,12 @@ class MessageListView(ListView):
     paginate_by = 10
     ordering = ["created_at"]
 
+    def get_queryset(self):
+        """ Вывод списка получателей рассылки как владельца или как администратора """
+        if self.request.user.is_superuser or self.request.user.is_staff:
+            return Message.objects.all()
+        return Message.objects.filter(owner=self.request.user)
+
 
 class MessageDetailView(DetailView):
     """ Класс просмотра деталей сообщении """
@@ -126,7 +174,7 @@ class MessageDetailView(DetailView):
     success_url = reverse_lazy("mailing_service:messages_list")
 
 
-class MessageCreateView(CreateView):
+class MessageCreateView(LoginRequiredMixin, CreateView):
     """ Класс создания сообщения """
     model = Message
     form_class = MessageForm
@@ -137,8 +185,16 @@ class MessageCreateView(CreateView):
         "object_type": "Message",
     }
 
+    def form_valid(self, form):
+        """ Создание текущего пользователя как владельца сообщения """
+        messages = form.save()
+        user = self.request.user
+        messages.owner = user
+        messages.save()
+        return super().form_valid(form)
 
-class MessageUpdateView(UpdateView):
+
+class MessageUpdateView(LoginRequiredMixin, UpdateView):
     """ Класс редактирования сообщения """
     model = Message
     form_class = MessageForm
@@ -150,7 +206,7 @@ class MessageUpdateView(UpdateView):
     }
 
 
-class MessageDeleteView(DeleteView):
+class MessageDeleteView(LoginRequiredMixin, DeleteView):
     """ Класс удаления сообщения """
     model = Message
     template_name = "mailing_service/confirm_delete.html"
@@ -170,12 +226,16 @@ class MailingListView(ListView):
     ordering = ["created_at"]
 
     def get_queryset(self):
-        queryset = super().get_queryset()
+        """ Вывод списка получателей рассылки как владельца или как администратора с обновлением статуса """
+        if self.request.user.is_superuser or self.request.user.is_staff:
+            queryset = Mailing.objects.all()
+        else:
+            queryset = Mailing.objects.filter(owner=self.request.user)
         
         for mailing in queryset:
             mailing.update_status()
 
-        return queryset
+        return queryset.order_by("-created_at")
 
 
 class MailingDetailView(DetailView):
@@ -185,12 +245,13 @@ class MailingDetailView(DetailView):
     context_object_name = "mailing"
 
     def get_object(self, queryset=None):
+        """ Обновление статуса в детализации рассылки """
         obj = super().get_object(queryset)
         obj.update_status()
         return obj
 
 
-class MailingCreateView(CreateView):
+class MailingCreateView(LoginRequiredMixin, CreateView):
     """ Класс создания рассылки """
     model = Mailing
     form_class = MailingForm
@@ -201,8 +262,23 @@ class MailingCreateView(CreateView):
         "object_type": "Mailing",
     }
 
+    def get_form(self, form_class=None):
+        """ Фильтруем выпадающие списки внутри формы """
+        form = super().get_form(form_class)
+        if not self.request.user.is_superuser:
+            form.fields["message"].queryset = Message.objects.filter(owner=self.request.user)
+            form.fields["recipients"].queryset = Message.objects.filter(owner=self.request.user)
 
-class MailingUpdateView(UpdateView):
+        return form
+
+
+    def form_valid(self, form):
+        """ Создание текущего пользователя как владельца рассылки """
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class MailingUpdateView(LoginRequiredMixin, UpdateView):
     """ Класс редактирования рассылки """
     model = Mailing
     form_class = MailingForm
@@ -214,7 +290,7 @@ class MailingUpdateView(UpdateView):
     }
 
 
-class MailingDeleteView(DeleteView):
+class MailingDeleteView(LoginRequiredMixin, DeleteView):
     """ Класс удаления рассылки """
     model = Mailing
     template_name = "mailing_service/confirm_delete.html"
@@ -240,3 +316,22 @@ class MailingStartView(View):
             messages.success(request, "Рассылка успешно отправлена получателям!")
 
         return redirect('mailing_service:mailings_list')
+
+class MailingDistributionView(LoginRequiredMixin, View):
+    """ Класс отключения рассылки """
+    def post(self, request, pk):
+        mailing = get_object_or_404(Mailing, pk=pk)
+        user = request.user
+
+        is_owner = (mailing.owner == user)
+        is_superuser = user.is_superuser
+        has_perms = user.has_perm("mailing.can_disable_distribution")
+
+        if not (is_owner or is_superuser or has_perms):
+            return HttpResponseForbidden("У вас не доступа для отключения рассылки.")
+
+        mailing.end_time = timezone.now()
+        messages.success(request, "Рассылка успешно отключена!")
+        mailing.save()
+
+        return redirect("mailing_service:mailings_list")
